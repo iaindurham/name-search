b98458548dc5fd6fd2429bc1933c824f5b472063
Iain Durham <iaindurham@gmail.com>
2016-07-21 10:02:13 +1000

    Put in filtering of results based on search string

--- routes/search.js
+++ routes/search.js
@@ -1,40 +1,53 @@
+var _ = require('lodash');
 var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
+  var people = [
+    {
+      id: '1',
+      name: 'eric',
+      image: 'images/avatars/eric.png'
+    },
+    {
+      id: '2',
+      name: 'hayley',
+      image: 'images/avatars/hayley.png'
+    },
+    {
+      id: '3',
+      name: 'lucy',
+      image: 'images/avatars/lucy.png'
+    },
+    {
+      id: '4',
+      name: 'rory',
+      image: 'images/avatars/rory.png'
+    },
+    {
+      id: '5',
+      name: 'sean',
+      image: 'images/avatars/sean.png'
+    },
+    {
+      id: '6',
+      name: 'yaw',
+      image: 'images/avatars/yaw.png'
+    }
+  ];
+  var searchResults = people;
 
-  var searchResults = {
-    results: [
-      {
-        name: 'eric',
-        image: 'images/avatars/eric.png'
-      },
-      {
-        name: 'hayley',
-        image: 'images/avatars/hayley.png'
-      },
-      {
-        name: 'lucy',
-        image: 'images/avatars/lucy.png'
-      },
-      {
-        name: 'rory',
-        image: 'images/avatars/rory.png'
-      },
-      {
-        name: 'sean',
-        image: 'images/avatars/sean.png'
-      },
-      {
-        name: 'yaw',
-        image: 'images/avatars/yaw.png'
-      }
-    ]
-  };
+  if (req.query) {
+    searchResults = _.filter(people, function (person) {
+      return person.name.match(req.query.search)
+    });
+  }
 
   res
     .type('json')
-    .send(searchResults);
+    .send({
+      results: searchResults
+    });
 });
 
 module.exports = router;
